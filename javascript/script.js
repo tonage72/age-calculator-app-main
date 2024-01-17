faa438b7c96b0348b7b2b4808d66dd67aeee6239
David Fleming <tonage72@gmail.com>
2024-01-16 19:52:23 -0500

    Working on the js

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,47 @@
 const submitButton = document.querySelector('.submit-button');
 const displayTest = document.querySelector('.display-test');
+const errorEmptyDay = document.querySelector('.error-empty-day');
+const errorEmptyMonth = document.querySelector('.error-empty-month');
+const errorEmptyYear = document.querySelector('.error-empty-year');
+const errorMonth = document.querySelector('.error-month');
+const errorDay = document.querySelector('.error-day');
+const errorYear = document.querySelector('.error-year');
+const errorDate = document.querySelector('.error-date');
+const outputMonths = document.querySelector('.output-months')
+const outputDays = document.querySelector('.output-days')
+const outputYears = document.querySelector('.output-years')
 
 submitButton.addEventListener("click", calculate);
 
 function calculate() {
 
-	day = document.getElementById("day").value;
-	month = document.getElementById("month").value;
-	year = document.getElementById("year").value;
+	const month = document.getElementById("month").value;
+	const day = document.getElementById("day").value;
+	const year = document.getElementById("year").value;
 
-	displayTest.innerHTML = day + month + year;
+	const wholeDate = month+day+year;
 
-	submitButton.style.backgroundColor = 'orange';
+	errorEmptyDay.style.display = "none";
+	errorEmptyMonth.style.display = "none";
+	errorEmptyYear.style.display = "none";
+	errorDay.style.display = "none";
+	errorMonth.style.display = "none";
+	errorYear.style.display = "none";
+	errorDate.style.display = "none"
+
+	if (!month || !day || !year) {
+		if (!month) {
+			errorEmptyMonth.style.display = "block";
+		}
+		if (!day) {
+			errorEmptyDay.style.display = "block";
+		}
+		if (!year) {
+			errorEmptyYear.style.display = "block";
+		}
+	} else {
+		outputYears.innerHTML = year;
+		outputMonths.innerHTML = month;
+		outputDays.innerHTML = day;
+	}
 }
